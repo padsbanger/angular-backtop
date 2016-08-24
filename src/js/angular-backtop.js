e50356993cce269c5ea6f85699e5ed3bf2fa2923
x0a <x0a.cake@gmail.com>
2016-08-24 15:27:57 -0500

    Fixed small error, and tabs

--- src/js/angular-backtop.js
+++ src/js/angular-backtop.js
@@ -15,63 +15,58 @@ backtop.directive('backTop', [function() {
 
       scope.text = scope.text || 'Scroll top';
       scope.theme = scope.theme || 'light';
-			scope.speed = parseInt(scope.speed, 10) || 300;
-			
-			var speed = Math.round(scope.speed / 100);
-			var onscroll = function() {
-				if (window.pageYOffset > 0) {
-					if(!element.showing){
-						element.addClass('show');
-						element.showing = true;
-					}
-				} else {
-					element.removeClass('show');
-					element.showing = false;
-				}
-			}
+      scope.speed = parseInt(scope.speed, 10) || 300;
+      
+      var speed = Math.round(scope.speed / 100);
+      var onscroll = function() {
+        if (window.pageYOffset > 0) {
+          if(!element.showing){
+            element.addClass('show');
+            element.showing = true;
+          }
+        } else {
+          element.removeClass('show');
+          element.showing = false;
+        }
+      }
 
-			scope.currentYPosition = function() {
-				if (document.documentElement && document.documentElement.scrollTop)
-					return document.documentElement.scrollTop;
-				if (document.body.scrollTop)
-					return document.body.scrollTop;
-				return 0;
-			};
+      scope.currentYPosition = function() {
+        if (document.documentElement && document.documentElement.scrollTop)
+          return document.documentElement.scrollTop;
+        if (document.body.scrollTop)
+          return document.body.scrollTop;
+        return 0;
+      };
 
-			element.showing = false;
-			element.on('click', function() {
-				window.removeEventListener("scroll", onscroll);
+      element.showing = false;
+      element.on('click', function() {
+        window.removeEventListener("scroll", onscroll);
 
-				element.removeClass('show');
-				element.showing = false;
+        element.removeClass('show');
+        element.showing = false;
 
-				var startY = scope.currentYPosition();
+        var startY = scope.currentYPosition();
 
-				if (startY < 100) {
-					scrollTo(0, 0);
-					return;
-				}
+        var step = Math.round(startY / 25);
+        var leapY = startY < 100 ? 0 : startY - step;
 
-				var step = Math.round(startY / 25);
-				var leapY = startY - step;
+        var scrollInterval = setInterval(function(){
+          window.scrollTo(0, leapY)
+          if(!leapY){
+            clearInterval(scrollInterval);
+            window.addEventListener("scroll", onscroll)
+          }
 
-				var scrollInterval = setInterval(function(){
-					window.scrollTo(0, leapY)
-					if(!leapY){
-						clearInterval(scrollInterval);
-						window.addEventListener("scroll", onscroll)
-					}
+          leapY -= step;
 
-					leapY -= step;
+          if(leapY < 0) leapY = 0;
+        }, speed)
+      });
 
-					if(leapY < 0) leapY = 0;
-				}, speed)
-			});
-
-			window.addEventListener("scroll", onscroll);
-			scope.$on("$destroy", function(){
-				window.removeEventListener("scroll", onscroll)
-			})
+      window.addEventListener("scroll", onscroll);
+      scope.$on("$destroy", function(){
+        window.removeEventListener("scroll", onscroll)
+      })
     }
   };
 
